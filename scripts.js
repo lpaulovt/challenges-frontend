ef395c3f3b0ea69215390fc35b3d3c7bfabefb84
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-30 18:58:28 -0300

    chore: animation overlay menu

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 document.querySelector('.nav-icon').addEventListener('click', function(){
     this.classList.toggle('active')
+    document.querySelector('.overlay-menu').classList.toggle('active')
 })
